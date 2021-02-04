dcf030524bda24c842adc15d5a3066c12700b9ec
Erik Fleckstein <erik@tinkerforge.com>
2021-02-04 12:01:42 +0100

    Fix spurious null pointer dereference.

--- src/AsyncEventSource.cpp
+++ src/AsyncEventSource.cpp
@@ -252,8 +252,9 @@ void AsyncEventSourceClient::_runQueue(){
 
   for(auto i = _messageQueue.begin(); i != _messageQueue.end(); ++i)
   {
-    if(!(*i)->sent())
+    if((*i) && !(*i)->sent()) {
       (*i)->send(_client);
+    }
   }
 }
 
